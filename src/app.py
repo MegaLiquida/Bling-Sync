aa0591860b18c5e195bff95c815e8a08d9cfcfab
MegaLiquida <marcos@megaliquidasp.com.br>
2025-07-07 14:45:53 -0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, redirect, url_for, render_template, send_from_
 from flask_session import Session
 import os
 import pandas as pd
-from .api_client import search_product, get_item_details, get_access_token, refresh_access_token
+from api_client import search_product, get_item_details, get_access_token, refresh_access_token
 from .data_processor import read_excel_data, write_excel_data, process_products
 from .config import CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, AUTH_URL, TOKEN_URL, API_BASE_URL, SECRET_KEY, UPLOAD_FOLDER, DATABASE_URL
 from dotenv import load_dotenv, set_key
