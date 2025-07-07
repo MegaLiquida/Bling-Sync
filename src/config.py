b34ef6ff7951345505f9dacdf2a18f873c81eda3
MegaLiquida <marcos@megaliquidasp.com.br>
2025-07-07 09:46:45 -0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
 import os
 
 # Credenciais da API do Mercado Livre
-CLIENT_ID = os.getenv("CLIENT_ID")
-CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+CLIENT_ID = os.getenv("7401826900082952")
+CLIENT_SECRET = os.getenv("AtsQ0fxExmiYTE8eE0bAWi1Q1yOL26Jv")
 REDIRECT_URI = os.getenv("REDIRECT_URI", "http://localhost:5000/callback")
 
 # Endpoints da API do Mercado Livre
